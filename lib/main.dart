950d2c78b985389c8b8fd640746b2af995d62fcb
Wantedbear007 <pratapsinghbhanu444@gmail.com>
2023-12-17 10:46:49 +0530

    :tada: Initial: added provider, path_provider as dependencies.

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:minute_player/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+      ),
+      home: HomeScreen(),
+      
+    );
+  }
+}
